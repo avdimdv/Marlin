e36825089f71f7fc6a41a7104fed8c039837c8cf
avdim <avdimdv@gmail.com>
2020-05-03 14:32:56 +0200

    Grid 7x7, Z offset tweak

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -975,7 +975,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -6, -2.82 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -6, -2.835 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1270,7 +1270,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 7
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
